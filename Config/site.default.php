53529e77c6ce6e834cc1b1ab91f0bba589ff34f7
kojoty <p.juzwiak@gmail.com>
2019-01-14 20:50:35 +0100

    site.default.php - default config for site

---
+++ Config/site.default.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Configuration of general site properties in defaut version
+ */
+
+
+$site = [];
+
+
+/**
+ * Primary country (or countries) for this node
+ */
+$site['primaryCountries'] = ['PL'];
+
